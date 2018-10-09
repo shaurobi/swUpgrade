04fdd0dc1a91e5f0c103c30430ea982f46c827d8
Shaun Robinson <shaurobi@users.noreply.github.com>
2018-10-09 20:30:19 +1100

    initial commit

---
+++ Upgrade0-1.py
@@ -0,0 +1,61 @@
+import os
+import platform
+import http.client
+
+def pushUpgrade(address):
+	conn = http.client.HTTPConnection("192.168.1.52")
+	payload = "<Command>\n\t<SystemUnit>\n\t\t<SoftwareUpgrade>\n\t\t\t<URL>\"http://192.168.1.49/s52030ce8_3_4.pkg\"</URL>\n\t\t</SoftwareUpgrade>\n\t</SystemUnit>\n</Command>"
+	headers = {
+	    'content-type': "text/xml",
+	    'cache-control': "no-cache",
+	    'postman-token': "23123b44-02ee-ee90-eb2d-146256e97fc8"
+	    }
+
+	conn.request("POST", "/putxml", payload, headers)
+	res = conn.getresponse()
+	data = res.read()
+	print(data.decode("utf-8"))
+
+def ping(address):
+	giveFeedback = False
+	if platform.system()== "Windows":
+		response = os.system("ping "+address+" -n 1")
+	else:
+		response = os.system("ping -c 1 " +address)
+		
+	if response == 0:
+		return True
+	else:
+		return False
+
+def resetCam(address):
+	conn = http.client.HTTPConnection(address)
+
+	payload = "<Command>\n\t<Camera>\n\t\t<PositionReset>\n\t\t\t<CameraId>1</CameraId>\n\t\t</PositionReset>\n\t</Camera>\n</Command>"
+
+	headers = {
+	    'content-type': "text/xml",
+	    'authorization': "Basic YWRtaW46",
+	    'cache-control': "no-cache",
+	    'postman-token': "1ba2946a-79fb-4810-ee8f-b9da63dc4f80"
+	    }
+
+	conn.request("POST", "/putxml", payload, headers)
+
+	res = conn.getresponse()
+	data = res.read()
+
+	print(data.decode("utf-8"))
+
+testaddy = "192.168.1.52"
+
+
+address = input("Please enter the IP Address of the endpoint in the format x.x.x.x:\n")
+if address == 0:
+	address = testaddy
+if ping(address) == True:
+	print("Address is reachable")
+else:
+	print("Address is unreachable, please check address and try again")
+	exit()
+
